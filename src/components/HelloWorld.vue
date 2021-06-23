a9b6c531dd0d6e485c74a80e4e2123bcef5866e5
lateofrederick <lateofrederick@gmail.com>
2021-06-23 19:20:11 +0000

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+  },
+})
+export default class HelloWorld extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+</style>
